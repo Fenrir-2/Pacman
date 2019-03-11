9361be699784db1df1fd1ac2308c423409642331
Nicolas <Nicolas@10.1.0.3>
2019-03-11 08:35:15 +0100

    Initial commit

---
+++ src/Level.java
@@ -0,0 +1,38 @@
+public class Level {
+	
+	private int index = 1;
+	
+	public Level() {
+		
+	}
+	
+	public void drawList() {
+		
+	}
+	
+	public void changeList() {
+		
+	}
+	
+	public void computeNextFrame() {
+		
+	}
+	
+	public void updateGhost() {
+		
+	}
+	
+	public void updatePacMan() {
+		
+	}
+	
+	public int checkEndGame() {
+		return 0; 
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
