105d1fbb90bf74609e418d1e1a55569b5fbd3365
Nicolas <e1602396@univ-ubs.fr>
2019-03-13 23:04:14 +0100

    Added definition for movement

--- src/Pacman/MoveableEntity.java
+++ src/Pacman/MoveableEntity.java
@@ -2,16 +2,31 @@ package Pacman;
 
 public class MoveableEntity extends Entity{
 	
+	/**
+	 * Constructor for the MoveableEntity 
+	 * 
+	 * @param x the x location of the entity
+	 * @param y the y location of the entity
+	 */
 	public MoveableEntity(int x, int y) {
 		super(x, y);
 	}
 	
+	/**
+	 * A FAIRE
+	 * @param gauche
+	 */
 	public void deplacementHorizontal(Boolean gauche) {
-		
+		this.posHor += gauche?-1:1;
 	}
 	
-	public void deplacementVertical(Boolean droite) {
-		
+	/**
+	 * A FAIRE
+	 * 
+	 * @param haut
+	 */
+	public void deplacementVertical(Boolean haut) {
+		this.posVer += haut?-1:1;
 	}
 	
 	
